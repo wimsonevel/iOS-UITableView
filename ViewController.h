3c4f587e7cd8fb0dc4461a9000d8e7274861ab99
Wim Sonevel <wim.sonevel@yahoo.co.id>
2016-05-09 20:24:03 +0700

    Add some

--- ViewController.h
+++ ViewController.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Item.h"
+#import "DetailController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 
 @end
